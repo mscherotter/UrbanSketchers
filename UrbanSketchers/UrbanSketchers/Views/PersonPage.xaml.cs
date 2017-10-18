7645928716136a3efc1b756d7e3a81fe65f63c23
Michael S. Scherotter <synergist@outlook.com>
2017-10-17 18:24:10 -0700

    updated person page

--- UrbanSketchers/UrbanSketchers/Views/PersonPage.xaml.cs
+++ UrbanSketchers/UrbanSketchers/Views/PersonPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using UrbanSketchers.Data;
+﻿using System;
+using UrbanSketchers.Commands;
+using UrbanSketchers.Data;
+using UrbanSketchers.Helpers;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,12 +13,59 @@ namespace UrbanSketchers.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PersonPage
     {
+        private readonly ViewModel _viewModel = new ViewModel();
+
         /// <summary>
         /// Initializes a new instance of the PersonPage class.
         /// </summary>
         public PersonPage()
         {
             InitializeComponent();
+
+            _viewModel.RefreshCommand = new RelayCommand<object>(OnRefresh, CanRefresh);
+            _viewModel.UpdateCommand = new RelayCommand<object>(OnUpdate, CanUpdate);
+            _viewModel.DeleteCommand = new RelayCommand<object>(OnDelete, CanDelete);
+            BindingContext = _viewModel;
+        }
+
+        private bool CanDelete(object arg)
+        {
+            return !_viewModel.IsBusy;
+        }
+
+        private async void OnDelete(object obj)
+        {
+            _viewModel.IsBusy = true;
+            await SketchManager.DefaultManager.DeleteCurrentUserAsync();
+            _viewModel.IsBusy = false;
+        }
+
+        private bool CanUpdate(object arg)
+        {
+            return !_viewModel.IsBusy;
+        }
+
+        private async void OnUpdate(object obj)
+        {
+            _viewModel.IsBusy = true;
+
+            await SketchManager.DefaultManager.SaveAsync(_viewModel.Person);
+
+            _viewModel.IsBusy = false;
+        }
+
+        private bool CanRefresh(object arg)
+        {
+            return !_viewModel.IsBusy;
+        }
+
+        private async void OnRefresh(object obj)
+        {
+            _viewModel.IsBusy = true;
+
+            await RefreshAsync();
+
+            _viewModel.IsBusy = false;
         }
 
         /// <summary>
@@ -30,7 +80,12 @@ namespace UrbanSketchers.Views
         {
             base.OnAppearing();
 
-            BindingContext = await SketchManager.DefaultManager.GetPersonAsync(PersonId);
+            await RefreshAsync();
+        }
+
+        private async System.Threading.Tasks.Task RefreshAsync()
+        {
+            _viewModel.Person = await SketchManager.DefaultManager.GetPersonAsync(PersonId);
 
             var sketches = await SketchManager.DefaultManager.GetSketchsAsync(PersonId);
 
@@ -51,6 +106,56 @@ namespace UrbanSketchers.Views
                     SketchId = sketch.Id
                 }, true);
             }
+        }
+
+        public class ViewModel : ObservableObject
+        {
+            private Person _person;
+            private RelayCommand<object> _refreshCommand;
+            private bool _isBusy;
+
+            public Person Person
+            {
+                get
+                {
+                    return _person;
+                }
+                set
+                {
+                    SetProperty(ref _person, value);
+                }
+            }
+
+            public RelayCommand<object> RefreshCommand
+            {
+                get
+                {
+                    return _refreshCommand;
+                }
+                set
+                {
+                    SetProperty(ref _refreshCommand, value);
+                }
+            }
+
+            public bool IsBusy
+            {
+                get
+                {
+                    return _isBusy;
+                }
+                internal set
+                {
+                    if (SetProperty(ref _isBusy, value))
+                    {
+                        RefreshCommand?.RaiseCanExecuteChanged();
+                        UpdateCommand?.RaiseCanExecuteChanged();
+                    }
+                }
+            }
+
+            public RelayCommand<object> UpdateCommand { get; internal set; }
+            public RelayCommand<object> DeleteCommand { get; internal set; }
         }
     }
 }
