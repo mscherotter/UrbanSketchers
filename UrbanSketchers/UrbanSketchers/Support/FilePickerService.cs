4b1364857b45d5f7eca1d82cf8d4af6c20ce2318
Michael S. Scherotter <synergist@outlook.com>
2017-11-20 09:32:16 -0800

    Added comments to file picker service

--- UrbanSketchers/UrbanSketchers/Support/FilePickerService.cs
+++ UrbanSketchers/UrbanSketchers/Support/FilePickerService.cs
@@ -9,20 +9,48 @@ namespace UrbanSketchers.Support
     /// </summary>
     public abstract class FilePickerService
     {
+        /// <summary>
+        /// the location Id
+        /// </summary>
         public enum LocationId
         {
+            /// <summary>
+            /// Pictures folder
+            /// </summary>
             Pictures,
+            /// <summary>
+            /// 3D Models folder
+            /// </summary>
             Models3D
         }
 
+        /// <summary>
+        /// The view mode
+        /// </summary>
         public enum ViewMode
         {
+            /// <summary>
+            /// List view
+            /// </summary>
             List,
+            /// <summary>
+            /// Thumbnail view
+            /// </summary>
             Thumbnail
         }
 
+        /// <summary>
+        /// Gets the singleton current file picker service
+        /// </summary>
         public static FilePickerService Current { get; set; }
 
+        /// <summary>
+        /// Pick a single file
+        /// </summary>
+        /// <param name="locationId">the starting location Id</param>
+        /// <param name="viewMode">the view mode</param>
+        /// <param name="fileTypeFilter">the file type filter</param>
+        /// <returns>an async task with the file data</returns>
         public abstract Task<FileData> PickOpenFileAsync(
             LocationId locationId,
             ViewMode viewMode,
