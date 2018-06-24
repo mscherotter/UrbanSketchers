087431a545323237c50ac0c835fd386e57ad732c
Michael S. Scherotter <synergist@outlook.com>
2018-06-23 17:19:26 -0700

    Added file title

---
+++ UrbanSketchers/UrbanSketchers/Data/PictureFileData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Plugin.FilePicker.Abstractions;
+
+namespace UrbanSketchers.Data
+{
+    public class PictureFileData : FileData
+    {
+        public string Title { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
