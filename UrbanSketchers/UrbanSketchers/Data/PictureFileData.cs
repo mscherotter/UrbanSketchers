b440402b7de878b548c4b937f871323f08962d55
Michael S. Scherotter <synergist@outlook.com>
2018-07-17 09:22:12 -0700

    added comments

--- UrbanSketchers/UrbanSketchers/Data/PictureFileData.cs
+++ UrbanSketchers/UrbanSketchers/Data/PictureFileData.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Plugin.FilePicker.Abstractions;
 
 namespace UrbanSketchers.Data
 {
+    /// <summary>
+    /// Picture file data
+    /// </summary>
     public class PictureFileData : FileData
     {
+        /// <summary>
+        /// Gets or sets the picture title from EXIF
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the latitude
+        /// </summary>
         public double? Latitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the longitude
+        /// </summary>
         public double? Longitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the creation date
+        /// </summary>
         public DateTime CreationDate { get; set; }
     }
 }
